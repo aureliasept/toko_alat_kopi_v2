4fd5b6d4f40c9e7c4292bf3470ff9036ddb50081
aureliasept <aureliaseptiani51@gmail.com>
2025-07-08 18:55:03 +0700

    menambahkan modal konfirmasi sebelum logout
    Menambahkan fitur modal konfirmasi saat pengguna klik "Logout", dengan pesan "Yakin ingin logout?" sebelum aksi logout diproses.

--- logout.php
+++ logout.php
@@ -1,10 +1,9 @@
 <?php
 session_start();
-
-// Destroy all session data
+// Hapus semua data session
+session_unset();
 session_destroy();
-
-// Redirect to login page
-header("Location: login.php");
+// Redirect ke halaman login
+header('Location: login.php');
 exit();
 ?> 
